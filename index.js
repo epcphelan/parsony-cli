69bb2e7b419c121c13007cf66529ef870e5a2a91
Eric Phelan <epcphelan@gmail.com>
2018-07-08 01:07:04 +0200

    fixed code styles

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-const program = require('commander');
-const {checkIsService, createService} = require('./libs/service');
-const {endpointWithService} = require('./libs/endpoint');
+const program = require("commander");
+const { checkIsService, createService } = require("./libs/service");
+const { endpointWithService } = require("./libs/endpoint");
 
 let service = null;
 let endpoint = null;
 
-program
-	.version('1.0.0')
-	.description('Parsony CLI');
+program.version("1.0.0").description("Parsony CLI");
 
 program
-	.command('addEndpoint')
-	.alias('+')
-	.description('Create a new endpoint')
-	.action(() => {
-		checkIsService((isService, serviceName) => {
-			if(!isService){
-				console.log('This service does not exist yet.');
-				createService(serviceName,(err)=>{
-					if(err){
-						console.log(err)
-					}
-					else{
-						endpointWithService(serviceName);
-					}
-				})
-			}
-			else{
-				endpointWithService(serviceName);
-			}
-		});
-	});
+  .command("addEndpoint")
+  .alias("+")
+  .description("Create a new endpoint")
+  .action(() => {
+    checkIsService((isService, serviceName) => {
+      if (!isService) {
+        console.log("This service does not exist yet.");
+        createService(serviceName, err => {
+          if (err) {
+            console.log(err);
+          } else {
+            endpointWithService(serviceName);
+          }
+        });
+      } else {
+        endpointWithService(serviceName);
+      }
+    });
+  });
 
 program.parse(process.argv);
